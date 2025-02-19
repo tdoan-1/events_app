82469d01f062f4ac8483045e035940ea3fc552cb
Cameron Tam <tamcameron2002@outlook.com>
2025-02-19 16:57:22 -0500

    initial commit

--- app.jsx
+++ app.jsx
@@ -1,1 +1,16 @@
+import React from "react";
+import header from 
+function App() {
+  return (
+    <div className="App">
+      <header />
+      <main>
+        <eventlist />
+      </main>
+      <footer />
+      <h1>Welcome to my webesite!</h1>
+    </div>
+  );
+}
 
+export default App;
--- main.jsx
+++ main.jsx
@@ -1,1 +1,11 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import App from './app.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
 
